aa8fe054e5c8f907aff32056fc4c075391235a6d
D0won <ldwlee03@gmail.com>
2024-09-06 11:14:46 +0900

    ldw/modify python file

--- Account_ldw.py
+++ Account_ldw.py
@@ -1,13 +1,13 @@
 import random
 
 class Account :
-    def __init__(self) :
+    def __init__(self) :  # 생성자로 모든 멤버변수를 초기화시킨다.
         self.name = input("이름이 무엇인가요?")
-        self.address = random.randrange(1000000000, 9999999999)
+        self.address = random.randrange(1000000000, 9999999999) # 10자리수를 랜덤하게 받는다.
         self.money = 0
     
-    def deposit(self) :
-        money = int(input("얼마를 입금하실건가요?"))
+    def deposit(self) : 
+        money = int(input("얼마를 입금하실건가요?")) 
         self.money += money
 
     def withdraw(self) :
@@ -17,14 +17,14 @@ class Account :
         else :
             print("출금할 돈이 모자랍니다.")
     
-    def send(self, address) :
+    def send(self, address) : # 다른 계좌(address)를 매개변수로 받아 송금시킨다.
         if self.money >= 0 :
             self.money -= address.money
             address.money += self.money
         else :
             print("돈이 없습니다.")
     
-    def accountinformation(self) :
+    def accountinformation(self) : # 계좌 정보를 출력한다.
         print("이름 : ", self.name)
         print("계좌번호 : ", self.address)
         print("잔고 : ", self.money)
--- main_ldw.py
+++ main_ldw.py
@@ -1,17 +1,18 @@
 import Account_ldw # Account 모듈 가져오기
 
-account = []
+account = [] # 계좌 저장 공간으로 계좌가 생성될 때마다 
 while True :
-    num = int(input("1 : 생성\n2 : 입금\n3 : 출금\n4 : 송금\n5 : 계좌정보조회\n나머지 번호: 제거"))
+    num = int(input("1 : 생성\n2 : 입금\n3 : 출금\n4 : 송금\n5 : 계좌정보조회\n나머지 번호: 종료"))
     if num == 1 : # 계좌 생성
         account.append(Account_ldw.Account())
-    elif num == 2 :
+
+    elif num == 2 : # 이름을 먼저 묻고 account 리스트에서 계좌를 찾은 다음 돈을 입력받아 입금시킨다.
         name = input("이름이 무엇인가요?")
         for i in range(len(account)) :
             if(account[i].name == name) :
                 account[i].deposit()
                 break;
-    elif num == 3 :
+    elif num == 3 : # 이름을 먼저 묻고 account 리스트에서 계좌를 찾은 다음 돈을 입력받아 출금시킨다. 만약에 계좌에 돈이 없으면 돈이 없다고 출력된다.
         name = input("이름이 무엇인가요?")
         for i in range(len(account)) :
             if(account[i].name == name) :
@@ -19,7 +20,8 @@ while True :
                 break;
             if(i == len(account) - 1) :
                 print("이름이 없습니다.")
-    elif num == 4 :
+
+    elif num == 4 : # 이름을 먼저 묻고 account 리스트에서 계좌를 찾은 다음 돈과 계좌이름을 입력받아 돈을 송금시킨다. 만약에 계좌에 돈이 없으면 돈이 없다고 출력된다.
         name = input("이름이 무엇인가요?")
         for i in range(len(account)) :
             if(account[i].name == name) :
@@ -36,10 +38,13 @@ while True :
                     break
             if(i == len(account) - 1) :
                 print("이름이 없습니다.")
-    elif num == 5 :
+    elif num == 5 : # 계좌 정보를 출력한다.
         name = input("이름이 무엇인가요?")
         for i in range(len(account)) :
             if(account[i].name == name) :
                 account[i].accountinformation()
+    else : 
+        print("종료되었습니다.")
+        break;
         
                 
